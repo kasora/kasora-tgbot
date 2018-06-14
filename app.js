595130e9f32c69a0dda53107e0afa5ff44968298
kasora <kasorasun@gmail.com>
2018-06-14 17:24:25 +0800

    [新增] log 输入信息

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ const cron = require('cron');
 Object.keys(routes).forEach((routeName) => {
   let route = routes[routeName]
   bot.onText(RegExp(`^\/${routeName}`), async (msg) => {
-    console.log(msg);
     await mongo.prepare();
     msg.command = utils.getCommand(msg.text);
     msg.bot = bot;
+    console.log(`username: `, msg.from.username);
+    console.log(`command : `, msg.text);
     try {
       let output = await route.handler(msg);
       msg.response = output;
@@ -30,5 +31,4 @@ var CronJob = require('cron').CronJob;
 new CronJob('0 * * * * *', async function () {
   await mongo.prepare();
   await alarmUtils.triggerAlarm();
-  console.log('alarm checked.')
 }, null, true, 'Asia/Shanghai', null, true);
